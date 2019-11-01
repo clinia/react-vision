37b10f50641aeef7f56ac454e934796318292751
Pedro Ferreira <pedro.ferreira@clinia.ca>
2019-11-01 17:54:43 -0400

    searchBox story board first interaction

--- stories/SearchBox.stories.js
+++ stories/SearchBox.stories.js
@@ -1,18 +1,68 @@
 import React, { Component } from 'react';
 import { storiesOf } from '@storybook/react';
-import { object, boolean } from '@storybook/addon-knobs';
+import { object, boolean, text } from '@storybook/addon-knobs';
 import { action } from '@storybook/addon-actions';
 import { SearchBox } from 'react-vision-dom';
 import { WrapWithHits } from './utils';
 
 const stories = storiesOf('SearchBox', module);
 
-stories.add('default', () => (
+stories.add('Default SearchBox', () => (
   <WrapWithHits
     searchBox={false}
     hasPlayground={true}
     linkedStoryGroup="SearchBox"
   >
-    <SearchBox showLoadingIndicator={boolean('showLoadingIndicator', true)} />
+    <SearchBox
+      showLoadingIndicator={boolean('showLoadingIndicator', true)}
+      loading={boolean('loading', true)}
+      searchAsYouType={boolean('searchAsYouType', true)}
+      placeholder={text('placeholder', '')}
+      onInput={action('onInput')}
+      onSubmit={action('onSubmit')}
+    />
+  </WrapWithHits>
+));
+
+const CustomButton = () => (
+  <button style={{ backgroundColor: 'blue', color: 'white' }}>
+    Custom Search Button
+  </button>
+);
+
+const CustomClearButton = ({ clearSearch }) => (
+  <button type="reset" onClick={clearSearch}>
+    Custom clear button
+  </button>
+);
+
+const CustomLoadingIndicator = () => (
+  <span style={{ backgroundColor: 'seagreen', width: '10px' }} />
+);
+
+stories.add('Custom SearchBox', () => (
+  <WrapWithHits
+    searchBox={false}
+    hasPlayground={true}
+    linkedStoryGroup="SearchBox"
+  >
+    <SearchBox
+      showLoadingIndicator={boolean('showLoadingIndicator', true)}
+      loading={boolean('loading', true)}
+      searchAsYouType={boolean('searchAsYouType', true)}
+      placeholder={text('placeholder', 'Custom placeholder')}
+      submitButton={<CustomButton />}
+      clearButton={clearSearch => (
+        <CustomClearButton clearSearch={clearSearch} />
+      )}
+      loadingIndicator={<CustomLoadingIndicator />}
+      onBlur={action('customOnBlur')}
+      onChange={action('customOnChange')}
+      onClear={action('customOnClear')}
+      onFocus={action('customOnFocus')}
+      onKeyPress={action('customOnKeyPress')}
+      onInput={action('customOnInput')}
+      onSubmit={action('customOnSubmit')}
+    />
   </WrapWithHits>
 ));
