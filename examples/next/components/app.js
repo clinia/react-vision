f6621e48c962c3e8fe5510a7a0867ec08ee5ebf7
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.ca>
2019-11-06 14:53:31 -0500

    starter for next

---
+++ examples/next/components/app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { SearchBox, Hits, Vision } from 'react-vision-dom';
+
+export default class extends React.Component {
+  static propTypes = {
+    searchState: PropTypes.object,
+    resultsState: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+    onSearchStateChange: PropTypes.func,
+    createURL: PropTypes.func,
+    indexName: PropTypes.string,
+    searchClient: PropTypes.object,
+  };
+
+  render() {
+    return (
+      <Vision
+        searchClient={this.props.searchClient}
+        resultsState={this.props.resultsState}
+        onSearchStateChange={this.props.onSearchStateChange}
+        searchState={this.props.searchState}
+        createURL={this.props.createURL}
+        indexName={this.props.indexName}
+        {...this.props}
+      >
+        <SearchBox />
+        <Hits />
+      </Vision>
+    );
+  }
+}
