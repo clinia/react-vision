ea4c95efb2893c9fc58b6234c6453e4a039ab75e
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.ca>
2019-11-06 13:40:18 -0500

    pr fix

--- packages/react-vision-dom/src/components/SearchBox.tsx
+++ packages/react-vision-dom/src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import React, { Component, RefObject } from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import LoadingIndicator from './LoadingIndicator';
 import { createClassNames } from '../core/utils';
