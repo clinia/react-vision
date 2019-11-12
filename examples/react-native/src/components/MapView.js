93d6b28e47b3532594e32555f736c275c66e5cb7
capitainecl <guillaume.poirier@clinia.ca>
2019-11-12 14:22:51 -0500

    feat: Added connectGeo to maps

--- examples/react-native/src/components/MapView.js
+++ examples/react-native/src/components/MapView.js
@@ -1,10 +1,16 @@
 import MapView, { Marker } from 'react-native-maps';
 import React from 'react';
-import { connectHits } from 'react-vision-native';
-import * as Permissions from 'expo-permissions';
-import * as Location from 'expo-location';
+import { connectGeoSearch } from 'react-vision-core';
 
 import { Container } from '../styles';
+
+// Default region is the city of Montreal QC, Canada.
+const defaultRegion = {
+  latitude: 45.53,
+  longitude: -73.56,
+  latitudeDelta: 0.5,
+  longitudeDelta: 0.5,
+};
 
 class Map extends React.Component {
   state = {
@@ -14,73 +20,55 @@ class Map extends React.Component {
   };
 
   componentDidMount() {
-    this.getLocationAsync();
+    const { refine } = this.props;
+    // Refine to default region
+    refine({
+      northEast: { lat: 45.53 + 0.25, lng: -73.56 + 0.25 },
+      southWest: { lat: 45.53 - 0.25, lng: -73.56 - 0.25 },
+    });
   }
 
-  getLocationAsync = async () => {
-    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+  onRegionChangeComplete = region => {
+    const { refine } = this.props;
+    const northEast = {
+      lat: region.latitude + region.latitudeDelta / 2,
+      lng: region.longitude + region.longitudeDelta / 2,
+    };
+    const southWest = {
+      lat: region.latitude - region.latitudeDelta / 2,
+      lng: region.longitude - region.longitudeDelta / 2,
+    };
 
-    let location = undefined;
-    if (status === 'granted') {
-      location = await Location.getCurrentPositionAsync();
-    }
-
-    this.setState({
-      showUserLocation: status === 'granted',
-      currentLocation: location &&
-        location.coords && {
-          latitude: location.coords.latitude,
-          longitude: location.coords.longitude,
-        },
-      hasAskedForLocationPermission: true,
+    refine({
+      northEast,
+      southWest,
     });
   };
 
   render() {
     const { records } = this.props;
-    const {
-      hasAskedForLocationPermission,
-      showUserLocation,
-      currentLocation,
-    } = this.state;
-
-    // Default region is the city of Montreal QC, Canada.
-    const region = {
-      latitude: 45.53,
-      longitude: -73.56,
-      latitudeDelta: 0.5,
-      longitudeDelta: 0.5,
-    };
-
-    if (currentLocation) {
-      region.latitude = currentLocation.latitude;
-      region.longitude = currentLocation.longitude;
-    }
-
     return (
-      hasAskedForLocationPermission && (
-        // We need to explicitly set the MapView size so it fills its container size.
-        <MapView
-          style={Container.content}
-          initialRegion={region}
-          showsUserLocation={showUserLocation}
-        >
-          {records.map(record => {
-            const { geoPoint } = record;
-            return (
-              <Marker
-                id={record.id}
-                key={record.id}
-                coordinate={{ latitude: geoPoint.lat, longitude: geoPoint.lng }}
-                title={record.name}
-                image={require('../../assets/pin.png')}
-              />
-            );
-          })}
-        </MapView>
-      )
+      // We need to explicitly set the MapView size so it fills its container size.
+      <MapView
+        style={Container.content}
+        initialRegion={defaultRegion}
+        onRegionChangeComplete={this.onRegionChangeComplete}
+      >
+        {records.map(record => {
+          const { geoPoint } = record;
+          return (
+            <Marker
+              id={record.id}
+              key={record.id}
+              coordinate={{ latitude: geoPoint.lat, longitude: geoPoint.lng }}
+              title={record.name}
+              image={require('../../assets/pin.png')}
+            />
+          );
+        })}
+      </MapView>
     );
   }
 }
 
-export default connectHits(Map);
+export default connectGeoSearch(Map);
