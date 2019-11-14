510548b78e2b5722542af4585faabe05a8301d8e
Etienne Soulard-Geoffrion <50806019+etiennecl@users.noreply.github.com>
2019-11-14 13:14:52 -0500

    feat: geo search widgets (maps, marker, redo, control) (#15)
    * fix: config of cliniasearch client for stories
    
    * Added geosearch stories

--- stories/utils.js
+++ stories/utils.js
@@ -1,75 +1,143 @@
-// import React, { useState, useMemo } from 'react';
-// import PropTypes from 'prop-types';
-// import { linkTo } from '@storybook/addon-links';
-// import cliniasearch from 'cliniasearch/lite';
-// import { Vision, SearchBox, connectHits } from 'react-vision-dom';
+import React, { useState, useMemo } from 'react';
+import PropTypes from 'prop-types';
+import { linkTo } from '@storybook/addon-links';
+import cliniasearch from 'cliniasearch/lite';
+import { Vision, Configure, SearchBox, connectHits } from 'react-vision-dom';
+import './style.css';
 
-// export const WrapWithHits = ({
-//   searchParameters: askedSearchParameters = {},
-//   children,
-//   searchBox = true,
-//   hasPlayground = false,
-//   linkedStoryGroup,
-//   pagination = true,
-//   appId,
-//   apiKey,
-//   indexName,
-//   hitsElement,
-//   initialSearchState,
-//   onSearchStateChange,
-// }) => {
-//   const searchClient = useMemo(() => {
-//     return cliniasearch(appId, apiKey);
-//   }, [appId, apiKey]);
+const Hits = ({ records }) => (
+  <div className="hits">
+    {records.map(record => (
+      <div key={record.id} className="record">
+        <div className="hit-content">
+          <div>
+            <h4>{record.name}</h4>
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
-//   // const hits = hitsElement || <CustomHits />;
+Hits.propTypes = {
+  records: PropTypes.array.isRequired,
+};
 
-//   const searchParameters = {
-//     perPage: 3,
-//     ...askedSearchParameters,
-//   };
+export const CustomHits = connectHits(Hits);
 
-//   const [searchState, setSearchState] = useState(initialSearchState);
+export const WrapWithHits = ({
+  searchParameters: askedSearchParameters = {},
+  children,
+  searchBox = true,
+  hasPlayground = false,
+  linkedStoryGroup,
+  pagination = true,
+  appId,
+  apiKey,
+  indexName,
+  hitsElement,
+  initialSearchState,
+  onSearchStateChange,
+}) => {
+  const searchClient = useMemo(() => {
+    return cliniasearch(appId, apiKey, {
+      hosts: {
+        write: ['api.partner.staging.clinia.ca'],
+        read: ['api.partner.staging.clinia.ca'],
+      },
+    });
+  }, [appId, apiKey]);
 
-//   const setNextSearchState = nextSearchState => {
-//     setSearchState(nextSearchState);
-//     onSearchStateChange(nextSearchState);
-//   };
+  const sourceCodeUrl = `https://github.com/clinia/react-vision/tree/develop/stories/${linkedStoryGroup}.stories.js`;
+  const playgroundLink = hasPlayground ? (
+    <button
+      onClick={linkTo(linkedStoryGroup, 'playground')}
+      className="playground-url"
+    >
+      <span>Play with props</span>
+    </button>
+  ) : null;
 
-//   return (
-//     <Vision
-//       searchClient={searchClient}
-//       indexName={indexName}
-//       searchState={searchState}
-//       onSearchStateChange={setNextSearchState}
-//     >
-//       <div>
-//         <div className="container widget-container">{children}</div>
-//         <div>{searchBox ? <></> : null}</div>
-//       </div>
-//     </Vision>
-//   );
-// };
+  const footer = linkedStoryGroup ? (
+    <div className="footer-container">
+      {playgroundLink}
+      <a
+        href={sourceCodeUrl}
+        className="source-code-url"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <div>View source code</div>
+      </a>
+    </div>
+  ) : null;
 
-// WrapWithHits.propTypes = {
-//   appId: PropTypes.string,
-//   apiKey: PropTypes.string,
-//   indexName: PropTypes.string,
-//   children: PropTypes.node,
-//   searchBox: PropTypes.bool,
-//   linkedStoryGroup: PropTypes.string,
-//   hasPlayground: PropTypes.bool,
-//   pagination: PropTypes.bool,
-//   searchParameters: PropTypes.object,
-//   hitsElement: PropTypes.element,
-//   initialSearchState: PropTypes.object,
-//   onSearchStateChange: PropTypes.func,
-// };
+  const hits = hitsElement || <CustomHits />;
 
-// WrapWithHits.defaultProps = {
-//   appId: 'TODO',
-//   apiKey: 'test',
-//   indexName: '',
-//   initialSearchState: {},
-//   onSearchStateChange: () => {},
-// };
+  const searchParameters = {
+    perPage: 3,
+    ...askedSearchParameters,
+  };
+
+  const [searchState, setSearchState] = useState(initialSearchState);
+
+  const setNextSearchState = nextSearchState => {
+    setSearchState(nextSearchState);
+    onSearchStateChange(nextSearchState);
+  };
+
+  return (
+    <Vision
+      searchClient={searchClient}
+      indexName={indexName}
+      searchState={searchState}
+      onSearchStateChange={setNextSearchState}
+    >
+      <Configure {...searchParameters} />
+      <div>
+        <div className="container widget-container">{children}</div>
+        <div>
+          <div
+            style={linkedStoryGroup ? {} : { borderRadius: '0px 0px 5px 5px' }}
+            className="container hits-container"
+          >
+            <div className="hit-actions">
+              {searchBox ? (
+                <SearchBox
+                  translations={{
+                    placeholder: 'Search into our products: phones, tv...',
+                  }}
+                />
+              ) : null}
+            </div>
+            {hits}
+          </div>
+          {footer}
+        </div>
+      </div>
+    </Vision>
+  );
+};
+
+WrapWithHits.propTypes = {
+  appId: PropTypes.string,
+  apiKey: PropTypes.string,
+  indexName: PropTypes.string,
+  children: PropTypes.node,
+  searchBox: PropTypes.bool,
+  linkedStoryGroup: PropTypes.string,
+  hasPlayground: PropTypes.bool,
+  pagination: PropTypes.bool,
+  searchParameters: PropTypes.object,
+  hitsElement: PropTypes.element,
+  initialSearchState: PropTypes.object,
+  onSearchStateChange: PropTypes.func,
+};
+
+WrapWithHits.defaultProps = {
+  appId: 'TODO',
+  apiKey: 'ClM5vDTmS4GWEL0aS7osJaRkowV8McuP',
+  indexName: 'health_facility',
+  initialSearchState: {},
+  onSearchStateChange: () => {},
+};
