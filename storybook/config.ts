08c1623c860f8748c158f36e5087bc03718d3b5e
Etienne Soulard-Geoffrion <etienne.soulard-geoffrion@clinia.ca>
2019-11-28 17:27:28 -0500

    fix: type-check & tests

--- storybook/config.ts
+++ storybook/config.ts
@@ -1,9 +1,7 @@
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withA11y } from '@storybook/addon-a11y';
-import { withTests } from '@storybook/addon-jest';
 import { create } from '@storybook/theming';
-import results from './.jest-test-results.json';
 
 addParameters({
   options: {
@@ -18,12 +16,6 @@ addParameters({
 
 addDecorator(withKnobs);
 addDecorator(withA11y);
-addDecorator(
-  withTests({
-    results,
-    filesExt: '.test.tsx',
-  })
-);
 
 const req = require.context('../stories', true, /\.stories\.(js|ts|tsx)$/);
 
